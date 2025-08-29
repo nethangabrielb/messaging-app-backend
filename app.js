f19b0084bec07fa7bfa334c435c8f1625efb63e5
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-08-29 08:46:27 +0800

    feat: initialized websockets

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import guestRouter from "./routes/guestRouter.js";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
 dotenv.config();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(cors());
 app.use(express.json());
@@ -14,4 +18,4 @@ app.use("/api", guestRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
