807e91e6afe65a8b2ec81bacb6ead487c5f5b8b4
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-04 11:20:09 +0800

    feat: initialized message event listeners

--- events/connectionHandler.js
+++ events/connectionHandler.js
@@ -1,7 +1,10 @@
 import initializeRoom from "./roomInitialization.js";
+import messageHandler from "./messageHandler.js";
 
 const onConnection = (socket) => {
   socket.on("create room", initializeRoom);
+
+  socket.on("message", messageHandler);
 };
 
 export default onConnection;
---
+++ events/messageHandler.js
@@ -0,0 +1,5 @@
+import messagesController from "../controllers/authenticated/messagesController.js";
+
+const messageHandler = () => {};
+
+export default messageHandler;
