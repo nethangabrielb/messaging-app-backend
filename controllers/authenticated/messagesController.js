6a220c211e3782b89e80da882e10857f381a6a1a
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-04 17:20:42 +0800

    feat: room message history fetching

--- controllers/authenticated/messagesController.js
+++ controllers/authenticated/messagesController.js
@@ -3,7 +3,23 @@ import { PrismaClient } from "../../generated/prisma/client.js";
 const prisma = new PrismaClient();
 
 const messagesController = (() => {
-  const getRoomMessages = () => {};
+  const getRoomMessages = async (req, res) => {
+    const { roomName } = req.params;
+
+    const room = await prisma.message.findMany({
+      where: {
+        room: {
+          name: roomName,
+        },
+      },
+    });
+
+    res.status(200).json({
+      code: "MESSAGES_RETRIEVED",
+      message: "Messages retrieved successfuly!",
+      data: room,
+    });
+  };
 
   return { getRoomMessages };
 })();
